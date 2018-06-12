49258127f97d89cf328591594b110078b4e2575f
Arthur McGregor <arthur.mcgregor@gmail.com>
2018-06-12 16:09:22 +1200

    Add visual form validation to registration form
    Fixes https://github.com/TheCacophonyProject/cacophony-web/issues/3
    (sort of)- Will highlightinput fields red when the input does not match
    theexpected pattern-Provides placeholder text to describe
    requirementsDoes NOT preventformsubmission (JS & API will still reject
    incorrectly submitted data)buthopefully this is sufficient feedback to
    reduce invalid submissions.

--- public/javascripts/register.js
+++ public/javascripts/register.js
@@ -2,6 +2,8 @@
 
 window.onload = function() {
   $("#register").click(register);
+  document.getElementById('password').addEventListener('input', passwordCheck);
+  document.getElementById('passwordRetype').addEventListener('input', passwordCheck);
 };
 
 function register() {
@@ -10,4 +12,14 @@ function register() {
     document.getElementById("passwordRetype"),
     document.getElementById("username")
   );
+}
+
+function passwordCheck() {
+  let password = document.getElementById('password');
+  let passwordRetype = document.getElementById('passwordRetype');
+  if (password.value === passwordRetype.value) {
+    passwordRetype.classList.remove('invalid');
+  } else {
+    passwordRetype.classList.add('invalid');
+  }
 }
